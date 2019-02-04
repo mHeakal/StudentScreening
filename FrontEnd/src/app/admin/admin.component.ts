ad74bd16ffac711965a7083297c63992b29ca00a
Ramesh Kumar <r.khatri91@gmail.com>
2019-02-04 13:51:27 -0600

    admin module in progress

---
+++ FrontEnd/src/app/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  tabNames= ["Staff", "Questions", "Answers"]
+  // constructor(private tabNames) {
+    // tabNames.push("Staff");
+    // tabNames.push("Questions");
+    // tabNames.push("Answers");
+  //  }
+
+  ngOnInit() {
+  }
+
+}
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { StaffListComponent } from './staff-list/staff-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    StaffListComponent
   ],
   imports: [
     BrowserModule,
