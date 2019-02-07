0efc13de6e3c08c55022cacabe00257c53c1e5c2
aliasadrizwan <ali.asad.rizwan@gmail.com>
2019-02-07 09:40:45 +0500

    backend update

--- backend/models/question.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = mongoose.Schema({
-    title: { type: String, require: true },
-    question: { type: String, required: true },
-    status: { type: Boolean, required: true }
-});
-
-module.exports = mongoose.model("Question", questionSchema);
