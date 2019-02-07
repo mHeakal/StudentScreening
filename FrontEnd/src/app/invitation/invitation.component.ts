1fd9930b01dc6ae397fc9b52d2cfb3afa4286f12
aliasadrizwan <ali.asad.rizwan@gmail.com>
2019-02-07 09:39:10 +0500

    Self closing message on invitation sent.

--- FrontEnd/src/app/invitation/invitation.component.ts
+++ FrontEnd/src/app/invitation/invitation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InvitationService, Student } from '../services/invitation.service';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-invitation',
@@ -8,10 +10,20 @@ import { InvitationService, Student } from '../services/invitation.service';
 })
 export class InvitationComponent implements OnInit {
   private students: Student[];
+  private _success = new Subject<string>();
+
+  staticAlertClosed = false;
+  successMessage: string;
   constructor(private invitationService: InvitationService) { }
 
   ngOnInit() {
     this.getAllStudents();
+    //Self closing alert message.
+    setTimeout(() => this.staticAlertClosed = true, 200);
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(2000)
+    ).subscribe(() => this.successMessage = null);
   }
 
   getAllStudents(){
@@ -25,6 +37,7 @@ export class InvitationComponent implements OnInit {
     this.invitationService.sendInvitation(student).subscribe(result => {
       //console.log("Invitation:"+ JSON.stringify(result));
       this.getAllStudents();
+      this._success.next(`Invitation successfully sent.`);
     });
   }
 
